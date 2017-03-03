4b700421f5f1d3c87776d48911e573dd0cea7c64
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-03 14:40:23 -0700

    ENH: hook up github bot credentials to bootstrapping pipeline

--- ci/bootstrap/scripts/deploy.sh
+++ ci/bootstrap/scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# IMPORTANT: Do not set -x to avoid leaking sensitive information below.
 set -e
+
 apt-get update
 apt-get install wget -y
 wget -O fly "$CONCOURSE_HOST/api/v1/cli?arch=amd64&platform=linux" --no-check-certificate
@@ -8,13 +10,11 @@ chmod +x ./fly
 
 ./fly -t qiime2 login -k -c $CONCOURSE_HOST -u $CONCOURSE_USER -p $CONCOURSE_PASS
 
-set -x
-
 for pipeline_path in busywork/ci/pipelines/*.yml
 do
   pipeline_name=$(basename "$pipeline_path")
   pipeline_name="${pipeline_name%.*}"
-  ./fly -t qiime2 set-pipeline -n -p $pipeline_name -c $pipeline_path
+  ./fly -t qiime2 set-pipeline -n -p $pipeline_name -c $pipeline_path --var 'github_user=$GITHUB_USER' --var 'github_pass=$GITHUB_PASS'
   ./fly -t qiime2 expose-pipeline -p $pipeline_name
   ./fly -t qiime2 unpause-pipeline -p $pipeline_name
 done
