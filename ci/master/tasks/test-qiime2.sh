69b1845c22b7e51822d0383a917ccb50700fa4e2
ebolyen <ebolyen@gmail.com>
2017-03-20 16:10:19 -0700

    set -e -x no yes

--- ci/master/tasks/test-qiime2.sh
+++ ci/master/tasks/test-qiime2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e -x
+
 conda create -y -n test-env
 source activate test-env
 conda install -y nose
-conda env update -y -f qiime2-source/ci/environment.yaml
+conda env update -f qiime2-source/ci/environment.yaml
 conda install -y -c ./builds -c qiime2 -c defaults --override-channels qiime2
 
 QIIMETEST= nosetests qiime2
