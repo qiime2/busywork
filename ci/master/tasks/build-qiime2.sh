c53c31c7cbbce25d9e401366bec0dfb6741250b4
ebolyen <ebolyen@gmail.com>
2017-03-20 15:15:19 -0700

    but yes sometimes

--- ci/master/tasks/build-qiime2.sh
+++ ci/master/tasks/build-qiime2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -x -e
+conda update -y conda
+conda update -y conda-build
 
 cd qiime2-source
 conda build -c https://conda.anaconda.org/qiime2 -c defaults --override-channels --output-folder ../builds --python 3.5 ci/recipe
