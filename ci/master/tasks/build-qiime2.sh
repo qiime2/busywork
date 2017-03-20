5e1dde67d08acfa32b446eeb48466ec1146d714d
ebolyen <ebolyen@gmail.com>
2017-03-20 15:07:50 -0700

    yes

--- ci/master/tasks/build-qiime2.sh
+++ ci/master/tasks/build-qiime2.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 set -x -e
-echo $PATH
-pwd
-ls
+
 cd qiime2-source
-pwd
-ls
-conda build -c https://conda.anaconda.org/qiime2 -c defaults --override-channels --output-folder ../builds --python 3.5 ci/recipe
-cd ..
-pwd
-ls
-find builds
+conda build -y -c https://conda.anaconda.org/qiime2 -c defaults --override-channels --output-folder ../builds --python 3.5 ci/recipe
+
+find ../builds
