8a99fe270e7533766e5a24020ad829a834c448e4
ebolyen <ebolyen@gmail.com>
2017-03-20 14:46:02 -0700

    debug

--- ci/master/tasks/build-qiime2.sh
+++ ci/master/tasks/build-qiime2.sh
@@ -3,3 +3,6 @@
 set -x -e
 echo $PATH
 conda build -c https://conda.anaconda.org/qiime2 -c defaults --override-channels --output-folder builds --python 3.5 qiime2-source/ci/recipe
+
+ls
+find builds
