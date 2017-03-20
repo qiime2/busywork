fa20a572e4008ae164081f6421258176586b9a2c
ebolyen <ebolyen@gmail.com>
2017-03-20 14:59:07 -0700

    cd

--- ci/master/tasks/build-qiime2.sh
+++ ci/master/tasks/build-qiime2.sh
@@ -3,8 +3,10 @@
 set -x -e
 echo $PATH
 pwd
+ls
 cd qiime2-source
 pwd
+ls
 conda build -c https://conda.anaconda.org/qiime2 -c defaults --override-channels --output-folder ../builds --python 3.5 ci/recipe
 cd ..
 pwd
