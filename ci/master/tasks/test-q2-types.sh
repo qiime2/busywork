f1f1a52a0520a9f43f68273ad73f1150244389bc
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-22 15:37:23 -0700

    WIP: q2-typesssssss

---
+++ ci/master/tasks/test-q2-types.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e -x
+
+conda update -y conda
+conda create -y -n test-env
+source activate test-env
+
+conda env update -f qiime2-source/ci/environment.yaml
+conda env update -f q2-types-source/ci/environment.yaml
+conda install -y pytest
+conda install -y -c ./builds -c ./qiime2-channel --override-channels q2-types
+
+py.test --pyargs q2_types
+
+source deactivate
+conda env remove -y -n test-env
