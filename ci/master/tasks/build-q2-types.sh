bd901e8c85f2cb12a4da21877700ff955d31677b
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-22 15:48:31 -0700

    WIP: debuggin'

--- ci/master/tasks/build-q2-types.sh
+++ ci/master/tasks/build-q2-types.sh
@@ -4,5 +4,14 @@ set -x -e
 conda update -y -c defaults --override-channels conda
 conda update -y -c defaults --override-channels conda-build
 
+echo "Hello, Evan"
+env
+conda info -a
+pwd
+ls
+ls qiime2-channel
+ls qiime2-channel/noarch
+ls builds
+
 cd q2-types-source
 conda build -c ../qiime2-channel -c qiime2 -c biocore -c defaults --override-channels --python 3.5 --output-folder ../builds ci/recipe
