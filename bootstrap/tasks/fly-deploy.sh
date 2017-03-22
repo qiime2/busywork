480055cae2e670584daec8272dffbe8f03530d37
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-22 14:39:10 -0700

    WIP: passin' vars around

--- bootstrap/tasks/fly-deploy.sh
+++ bootstrap/tasks/fly-deploy.sh
@@ -19,7 +19,7 @@ do
     pipeline_name=$(basename "$pipeline_path")
     pipeline_name="${pipeline_name%.*}"
     pipeline_name="$product_name-$pipeline_name"
-    ./fly -t qiime2 set-pipeline -n -p $pipeline_name -c $pipeline_path
+    ./fly -t qiime2 set-pipeline -n -p $pipeline_name -c $pipeline_path --var "github_user=$GITHUB_USER" --var "github_pass=$GITHUB_PASS" --var "ftp_host=$FTP_HOST" --var "ftp_channel_root=$FTP_CHANNEL_ROOT" --var "ftp_user=$FTP_USER" --var "ftp_pass=$FTP_PASS"
     ./fly -t qiime2 expose-pipeline -p $pipeline_name
     ./fly -t qiime2 unpause-pipeline -p $pipeline_name
   done
