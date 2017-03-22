e6f9e6ee31ff5c6f5ed671972bc1b300a5252771
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-22 14:26:10 -0700

    WIP: try out new conda channel resource type!

--- bin/deploy-bootstrap.sh
+++ bin/deploy-bootstrap.sh
@@ -13,6 +13,11 @@ concourse_user=$(grep "auth_user" $secrets_repo/ansible_hosts/concourse/host_var
 concourse_user=$(grep "auth_user" $secrets_repo/ansible_hosts/concourse/host_vars/powertrip.yml | sed "s/auth_user: //g")
 concourse_pass=$(grep "auth_pass" $secrets_repo/ansible_hosts/concourse/host_vars/powertrip.yml | sed "s/auth_pass: //g")
 
+ftp_host=$(grep "ftp_host" $secrets_repo/ansible_hosts/concourse/host_vars/toast.yml | sed "s/ftp_host: //g")
+ftp_channel_root=$(grep "ftp_channel_root" $secrets_repo/ansible_hosts/concourse/host_vars/toast.yml | sed "s/ftp_channel_root: //g")
+ftp_user=$(grep "ftp_user" $secrets_repo/ansible_hosts/concourse/host_vars/toast.yml | sed "s/ftp_user: //g")
+ftp_pass=$(grep "ftp_pass" $secrets_repo/ansible_hosts/concourse/host_vars/toast.yml | sed "s/ftp_pass: //g")
+
 github_user=$(head -n1 $secrets_repo/keys/q2d2/github.csv | cut -f1 -d',')
 github_pass=$(head -n1 $secrets_repo/keys/q2d2/github.csv | cut -f2 -d',')
 
@@ -20,5 +25,9 @@ fly -t qiime2 set-pipeline -p bootstrap -c bootstrap/pipelines/deploy.yaml \
     --var "concourse_host=$concourse_host" \
     --var "concourse_user=$concourse_user" \
     --var "concourse_pass=$concourse_pass" \
+    --var "ftp_host=$ftp_host" \
+    --var "ftp_channel_root=$ftp_channel_root" \
+    --var "ftp_user=$ftp_user" \
+    --var "ftp_pass=$ftp_pass" \
     --var "github_user=$github_user" \
     --var "github_pass=$github_pass"
