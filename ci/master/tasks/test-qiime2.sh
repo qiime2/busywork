81f71dfff7568ad91207e4600592c34568762593
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-23 15:41:49 -0700

    WIP: qiime2: less source

--- ci/master/tasks/test-qiime2.sh
+++ ci/master/tasks/test-qiime2.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 set -e -x
-env
-ls
-ls qiime2-source
-ls builds
+
 conda update -y conda
 
 conda create -y -n test-env
 source activate test-env
-conda env update -f qiime2-source/ci/environment.yaml
 conda install -y nose
 conda install -y -c ./builds -c qiime2 -c defaults --override-channels qiime2
 
