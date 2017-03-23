de90ac302b27417a519ae3a3f540702cd5dd6b4a
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-23 12:53:34 -0700

    WIP: q2-feature-table!

---
+++ ci/master/tasks/build-q2-feature-table.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x -e
+conda update -y -c defaults --override-channels conda
+conda update -y -c defaults --override-channels conda-build
+
+cd q2-feature-table-source
+git fetch --tags
+conda build -c ../q2-types-channel -c ../q2templates-channel -c ../qiime2-channel -c qiime2 -c biocore -c defaults --override-channels --python 3.5 --output-folder ../builds ci/recipe
