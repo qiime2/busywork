4689a644cbec56d761d073bc86a2123f728cd1c8
ebolyen <ebolyen@gmail.com>
2017-03-28 15:54:19 -0700

    WIP: q2cli

--- ci/master/tasks/q2cli/test.sh
+++ ci/master/tasks/q2cli/test.sh
@@ -6,7 +6,7 @@ conda update -y conda
 conda create -y -p ./test-env
 source activate ./test-env
 
-conda install -y -c ./builds -c ./qiime2-channel -c qiime2 -c defaults --override-channels q2cli
+conda install -y -c ./q2cli-channel -c ./qiime2-channel -c qiime2 -c defaults --override-channels q2cli
 conda install -y nose
 echo "backend: Agg" > matplotlibrc
 
