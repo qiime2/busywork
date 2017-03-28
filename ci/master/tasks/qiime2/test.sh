67071a1a6a0fb16c462aa654d23e5bf38b9740d7
ebolyen <ebolyen@gmail.com>
2017-03-28 11:32:57 -0700

    WIP: debug ls

--- ci/master/tasks/qiime2/test.sh
+++ ci/master/tasks/qiime2/test.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 conda update -y conda
-
+ls builds
 conda create -y -p ./test-env
 source activate ./test-env
 conda install -y -c ./builds -c qiime2 -c defaults --override-channels qiime2
