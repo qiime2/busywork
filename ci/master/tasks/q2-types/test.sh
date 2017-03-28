b09407b584ae1ac109acdb204a0a20685573abb9
ebolyen <ebolyen@gmail.com>
2017-03-28 16:19:21 -0700

    WIP: q2-types

--- ci/master/tasks/q2-types/test.sh
+++ ci/master/tasks/q2-types/test.sh
@@ -6,7 +6,7 @@ conda update -y conda
 conda create -y -p ./test-env
 source activate ./test-env
 
-conda install -y -c ./builds -c ./qiime2-channel -c qiime2 -c biocore -c defaults --override-channels q2-types
+conda install -y -c ./q2-types-channel -c ./qiime2-channel -c qiime2 -c biocore -c defaults --override-channels q2-types
 conda install -y pytest
 echo "backend: Agg" > matplotlibrc
 
