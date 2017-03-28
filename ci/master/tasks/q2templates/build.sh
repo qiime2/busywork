4a4254392df9089c273fec0b117355b218b2f90c
ebolyen <ebolyen@gmail.com>
2017-03-28 16:05:40 -0700

    WIP: q2templates

--- ci/master/tasks/q2templates/build.sh
+++ ci/master/tasks/q2templates/build.sh
@@ -4,6 +4,7 @@ set -x -e
 conda update -y -c defaults --override-channels conda
 conda update -y -c defaults --override-channels conda-build
 
+BUILD_DIR=$(ls -d -1 $(pwd)/build-*)
 cd q2templates-source
 git fetch --tags
-conda build -c defaults --override-channels --output-folder ../builds --python 3.5 ci/recipe
+conda build -c defaults --override-channels --output-folder $BUILD_DIR --python 3.5 ci/recipe
