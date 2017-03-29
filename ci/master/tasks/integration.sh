d25aa67dc06e2213797ea8250888f7ee1f466fd5
Jai Ram Rideout <jai.rideout@gmail.com>
2017-03-29 15:20:24 -0700

    MAINT: q2-diversity

--- ci/master/tasks/integration.sh
+++ ci/master/tasks/integration.sh
@@ -13,7 +13,7 @@ conda install -y $CHANNELS \
   -c defaults \
   -c https://conda.anaconda.org/bioconda \
   --override-channels \
-  qiime2 q2cli q2templates q2-types q2-feature-table q2-alignment q2-composition q2-deblur q2-demux
+  qiime2 q2cli q2templates q2-types q2-feature-table q2-alignment q2-composition q2-deblur q2-demux q2-diversity
 
 echo "backend: Agg" > matplotlibrc
 
