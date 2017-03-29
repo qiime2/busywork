7932527be9104a4d67e1d2242ae42363a2944f62
ebolyen <ebolyen@gmail.com>
2017-03-29 12:42:22 -0700

    WIP: add generic testing?

---
+++ ci/master/tasks/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e -x
+
+conda update -y conda
+conda create -y -p ./test-env
+source activate ./test-env
+
+CHANNELS=$(ls -d -1 $(pwd)/*-channel | sed -s "s/^/ -c /" | xargs) || ''
+conda install -y -c $CHANNELS -c qiime2 -c biocore -c defaults --override-channels $PKG_NAME
+conda install -y pytest
+echo "backend: Agg" > matplotlibrc
+
+$TEST_RUNNER_CMD $PKG_NAME
