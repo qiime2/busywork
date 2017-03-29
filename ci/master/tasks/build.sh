7d113c7bb1cecb26aee47de218f6c09c22c56514
ebolyen <ebolyen@gmail.com>
2017-03-29 13:07:46 -0700

    WIP: url channel

--- ci/master/tasks/build.sh
+++ ci/master/tasks/build.sh
@@ -9,4 +9,4 @@ BUILD_DIR=$(ls -d -1 $(pwd)/build-*)
 CHANNELS=$(ls -d -1 $(pwd)/*-channel | sed "s/^/ -c /" | xargs) || ''
 cd ./source
 git fetch --tags
-conda build $CHANNELS -c qiime2 -c biocore -c defaults --override-channels --python 3.5 --output-folder $BUILD_DIR ci/recipe
+conda build $CHANNELS -c https://conda.anaconda.org/qiime2 -c biocore -c defaults --override-channels --python 3.5 --output-folder $BUILD_DIR ci/recipe
--- ci/master/tasks/test.sh
+++ ci/master/tasks/test.sh
@@ -7,7 +7,7 @@ conda create -y -p ./test-env
 source activate ./test-env
 
 CHANNELS=$(ls -d -1 $(pwd)/*-channel | sed "s/^/ -c /" | xargs) || ''
-conda install -y $CHANNELS -c qiime2 -c biocore -c defaults --override-channels $PKG_NAME
+conda install -y $CHANNELS -c https://conda.anaconda.org/qiime2 -c biocore -c defaults --override-channels $PKG_NAME
 conda install -y pytest
 echo "backend: Agg" > matplotlibrc
 
