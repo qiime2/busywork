dccd9de8a98a9d6d861a263e2667897b0982cbfc
ebolyen <ebolyen@gmail.com>
2017-03-29 13:33:55 -0700

    WIP: updates

--- ci/master/tasks/build.sh
+++ ci/master/tasks/build.sh
@@ -6,7 +6,7 @@ conda update -y -c defaults --override-channels conda
 conda update -y -c defaults --override-channels conda-build
 
 BUILD_DIR=$(ls -d -1 $(pwd)/build-*)
-CHANNELS=$(ls -d -1 $(pwd)/*-channel | sed "s/^/ -c /" | xargs) || ''
+CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
 cd ./source
 git fetch --tags
 conda build $CHANNELS -c https://conda.anaconda.org/qiime2 -c biocore -c defaults --override-channels --python 3.5 --output-folder $BUILD_DIR ci/recipe
--- ci/master/tasks/test.sh
+++ ci/master/tasks/test.sh
@@ -6,9 +6,10 @@ conda update -y conda
 conda create -y -p ./test-env
 source activate ./test-env
 
-CHANNELS=$(ls -d -1 $(pwd)/*-channel | sed "s/^/ -c /" | xargs) || ''
+CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
 conda install -y $CHANNELS -c https://conda.anaconda.org/qiime2 -c biocore -c defaults --override-channels $PKG_NAME
 conda install -y pytest
+conda install -y nose
 echo "backend: Agg" > matplotlibrc
 
 $TEST_RUNNER_CMD
