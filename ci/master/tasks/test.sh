a7e155f7eca1f0e8827a355c1dd0108a4c58e8fc
ebolyen <ebolyen@gmail.com>
2017-04-03 11:47:39 -0700

    WIP: optimistic refactor

--- ci/master/tasks/test.sh
+++ ci/master/tasks/test.sh
@@ -2,22 +2,11 @@
 
 set -e -v
 
-conda update -q -y conda
-conda create -q -y -p ./test-env
-
 set +v
-echo "source activate ./test-env"
-source activate ./test-env
+echo "source activate ./conda-env"
+source activate ./conda-env
 set -v
 
-CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
-conda install -q -y $CHANNELS \
-  -c https://conda.anaconda.org/qiime2 \
-  -c https://conda.anaconda.org/biocore \
-  -c defaults \
-  -c https://conda.anaconda.org/conda-forge \
-  -c https://conda.anaconda.org/bioconda \
-  --override-channels $PKG_NAME pytest nose
 echo "backend: Agg" > matplotlibrc
 
 $TEST_RUNNER_CMD
