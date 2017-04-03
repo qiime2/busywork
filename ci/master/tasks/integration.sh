e0b54a7536662a8206925a85f0aa05dbe26d65f4
ebolyen <ebolyen@gmail.com>
2017-04-03 12:43:05 -0700

    Revert "WIP: optimistic refactor"
    This reverts commit a7e155f7eca1f0e8827a355c1dd0108a4c58e8fc.

--- ci/master/tasks/integration.sh
+++ ci/master/tasks/integration.sh
@@ -9,10 +9,38 @@ then
   apt-get install wget -q -y --allow-unauthenticated # Really??
 fi
 
+conda update -q -y conda
+conda create -q -y -p ./test-env
+
 set +v
-echo "source activate ./conda-env"
-source activate ./conda-env
+echo "source activate ./test-env"
+source activate ./test-env
 set -v
+
+CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
+conda install -q -y $CHANNELS \
+  -c https://conda.anaconda.org/qiime2 \
+  -c https://conda.anaconda.org/biocore \
+  -c defaults \
+  -c https://conda.anaconda.org/conda-forge \
+  -c https://conda.anaconda.org/bioconda \
+  --override-channels \
+  qiime2 \
+  q2cli \
+  q2templates \
+  q2-types \
+  q2-feature-table \
+  q2-alignment \
+  q2-composition \
+  q2-dada2 \
+  q2-deblur \
+  q2-demux \
+  q2-diversity \
+  q2-emperor \
+  q2-feature-classifier \
+  q2-phylogeny \
+  q2-quality-filter \
+  q2-taxa
 
 cd docs-source
 pip install -q -r requirements.txt
--- ci/master/tasks/test.sh
+++ ci/master/tasks/test.sh
@@ -2,11 +2,22 @@
 
 set -e -v
 
+conda update -q -y conda
+conda create -q -y -p ./test-env
+
 set +v
-echo "source activate ./conda-env"
-source activate ./conda-env
+echo "source activate ./test-env"
+source activate ./test-env
 set -v
 
+CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
+conda install -q -y $CHANNELS \
+  -c https://conda.anaconda.org/qiime2 \
+  -c https://conda.anaconda.org/biocore \
+  -c defaults \
+  -c https://conda.anaconda.org/conda-forge \
+  -c https://conda.anaconda.org/bioconda \
+  --override-channels $PKG_NAME pytest nose
 echo "backend: Agg" > matplotlibrc
 
 $TEST_RUNNER_CMD
