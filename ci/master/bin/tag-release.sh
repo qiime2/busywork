7bbd8a8e2bdeb03d429f667e23482dc33b7fde56
Jai Ram Rideout <jai.rideout@gmail.com>
2017-04-14 16:49:30 -0700

    WIP: bash programming 101

--- ci/master/bin/tag-release.sh
+++ ci/master/bin/tag-release.sh
@@ -5,9 +5,11 @@
 
 set -e
 
+# Necessary for pushing (configures curl and other networking tools).
 rm -f $HOME/.netrc
 echo "default login $GITHUB_USER password $GITHUB_PASS" > $HOME/.netrc
 
+# NOTE: Do not echo commands above to avoid leaking secrets.
 set -v
 
 # Fix this someday
@@ -22,28 +24,49 @@ apt-get install -y git
 git config --global user.name q2d2
 git config --global user.email "q2d2.noreply@gmail.com"
 
+# Convert a space-separated string to an array.
 REPOS_ARRAY=(${REPOS_STRING[*]})
 
-echo $REPOS_ARRAY
-echo ${REPOS_ARRAY[*]}
-echo ${REPOS_ARRAY[0]}
-echo ${REPOS_ARRAY[1]}
-echo ${REPOS_ARRAY[2]}
+# Ask for current release in the current working directory.
+_get_release() {
+  python -c 'import versioneer; version = versioneer.get_version(); release = ".".join(version.split(".")[:2]); print(release)'
+}
 
-exit 1
+# Taken from http://stackoverflow.com/a/3352015/3776794
+trim() {
+  local var="$*"
+  # remove leading whitespace characters
+  var="${var#"${var%%[![:space:]]*}"}"
+  # remove trailing whitespace characters
+  var="${var%"${var##*[![:space:]]}"}"
+  echo -n "$var"
+}
 
-#git clone qiime2 qiime2-feature-freeze
-#cd qiime2-feature-freeze
-#git remote remove origin
-#git remote add origin $(cd ../qiime2 && git remote get-url --push origin)
-#
-#next_release="$(cat ../busywork/next-release)"
-#current_release="$(grep 'version=' setup.py | sed -E "s/^.*version=['\"](.+)['\"].*$/\1/" | cut -f1,2 -d'.')"
-#next_version=$next_release.0.dev0
-#git branch dev/$current_release
-#git checkout dev/main
-#sed -i "s/version=.*/version='$next_version',/g" setup.py
-#git add setup.py
-#git commit -m "VER: $next_version"
-#git push origin dev/main
-#git push origin dev/$current_release
+expected_release="$(trim $(cat busywork/current-dev-release))"
+
+for repo in $REPOS_ARRAY
+do
+  cp -r $repo-source tagged-$repo-source
+  cd tagged-$repo-source
+
+  observed_release=$(_get_release)
+
+  if [ "$observed_release" != "$expected_release" ]
+  then
+    echo "Repo $repo has current dev release $observed_release but busywork/current-dev-release declares $expected_release."
+    rm -f $HOME/.netrc
+    exit 1
+  fi
+
+#  current_release="$(grep 'version=' setup.py | sed -E "s/^.*version=['\"](.+)['\"].*$/\1/" | cut -f1,2 -d'.')"
+#  next_version=$next_release.0.dev0
+#  git branch dev/$current_release
+#  git checkout dev/main
+#  sed -i "s/version=.*/version='$next_version',/g" setup.py
+#  git add setup.py
+#  git commit -m "VER: $next_version"
+#  git push origin dev/main
+#  git push origin dev/$current_release
+done
+
+rm -f $HOME/.netrc
