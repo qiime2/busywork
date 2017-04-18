b16cf59ff52d4a7e4ce232bb7dc4f11d4f61e168
Jai Ram Rideout <jai.rideout@gmail.com>
2017-04-18 16:47:08 -0700

    MAINT: remove provisioning directory

--- provisioning/roles/setup_darwin_worker/templates/run_worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/usr/local/bin/concourse worker \
-  --work-dir {{ worker_dir }} \
-  --tsa-host {{ tsa_host }} \
-  --tsa-port {{ tsa_port }} \
-  --tsa-public-key {{ base_dir }}/{{ keys_dir }}/tsa_host_key.pub \
-  --tsa-worker-private-key {{ base_dir }}/{{ keys_dir }}/worker_key
