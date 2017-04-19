8f689e27d9c03279fb7c26a9d2aa959227360fc6
Matthew Dillon <matthewrdillon@gmail.com>
2017-04-19 15:44:06 -0700

    ENH: guard that stuff, like whoa

--- ci/master/bin/merge-env-files.sh
+++ ci/master/bin/merge-env-files.sh
@@ -15,10 +15,13 @@ mkdir -p $RELPATH
 cp ../linux-environment-files/* $RELPATH
 cp ../darwin-environment-files/* $RELPATH
 
-# Latest Staging
-mkdir -p latest/staging
-cp ../linux-environment-files/* latest/staging
-cp ../darwin-environment-files/* latest/staging
+if [ "$BUILD_TYPE" == "staging" ]
+then
+  # Latest Staging
+  mkdir -p latest/staging
+  cp ../linux-environment-files/* latest/staging
+  cp ../darwin-environment-files/* latest/staging
+fi
 
 git add $RELPATH
 git diff-index --quiet HEAD || git commit -m "Updated $RELEASE $BUILD_TYPE environment file."
