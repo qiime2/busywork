f5dc64a57b8245f1affd5088bbceb86cdbe23457
Evan Bolyen <ebolyen@gmail.com>
2017-09-19 15:17:13 -0700

    MAINT: prefer conda-forge

--- ci/master/bin/build.sh
+++ ci/master/bin/build.sh
@@ -11,8 +11,8 @@ cd ./source
 git fetch --tags
 conda build -q $CHANNELS \
   -c https://conda.anaconda.org/qiime2 \
-  -c defaults \
   -c https://conda.anaconda.org/conda-forge \
+  -c defaults \
   -c https://conda.anaconda.org/bioconda \
   -c https://conda.anaconda.org/biocore \
   --override-channels --python 3.5 --output-folder $BUILD_DIR ci/recipe
