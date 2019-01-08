bd382cc59ac611d30d2080829ccf41a8827ae36f
Matthew Dillon <matthewrdillon@gmail.com>
2019-01-08 13:18:53 -0700

    ENH: debugging envs

--- ci/master/bin/integration.sh
+++ ci/master/bin/integration.sh
@@ -21,6 +21,8 @@ echo "source activate ./test-env"
 source activate ./test-env
 set -v
 
+# debug-env.yml for when this task fails, allows us to recreate the working env.
+conda list --explicit --export > debug-env.yml
 conda env export --no-builds --ignore-channels -p ./test-env > $ENV_FILE_FP
 
 cd docs-source
