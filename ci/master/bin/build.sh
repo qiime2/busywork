1fe32b70f7558903612ea713ab86bee7ef622de1
Matthew Dillon <matthewrdillon@gmail.com>
2019-01-09 15:54:29 -0700

    MAINT: Python 3.5 -> 3.7

--- ci/master/bin/build.sh
+++ ci/master/bin/build.sh
@@ -10,9 +10,8 @@ CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
 cd ./source
 git fetch --tags
 conda build -q $CHANNELS \
-  -c https://conda.anaconda.org/qiime2 \
   -c https://conda.anaconda.org/conda-forge \
   -c defaults \
   -c https://conda.anaconda.org/bioconda \
   -c https://conda.anaconda.org/biocore \
-  --override-channels --python 3.5 --output-folder $BUILD_DIR ci/recipe
+  --override-channels --output-folder $BUILD_DIR ci/recipe
