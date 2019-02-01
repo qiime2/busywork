0c40aa831a1da6b98e53b46c8df222721dbb2032
Matthew Dillon <matthewrdillon@gmail.com>
2019-02-01 12:22:56 -0700

    HACK: pin conda to 4.5

--- ci/master/bin/build.sh
+++ ci/master/bin/build.sh
@@ -2,10 +2,7 @@
 
 set -e -v
 
-# tell conda to "relax" (https://github.com/conda/conda/issues/7788#issuecomment-446051125)
-echo "allow_conda_downgrades: true" >> ~/.condarc
-
-conda upgrade -q -y -c defaults --override-channels conda
+conda install -q -y -c defaults --override-channels conda=4.5
 conda install -q -y -c defaults --override-channels conda-build conda-verify
 
 BUILD_DIR=$(ls -d -1 $(pwd)/build-*)
