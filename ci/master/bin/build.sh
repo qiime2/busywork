8edcd9f7cc88f3ee2104b5984fb7b10fce13ab68
Matthew Dillon <matthewrdillon@gmail.com>
2019-02-12 15:44:59 -0700

    HACK: bootstrap with bioconda reqs

--- ci/master/bin/build.sh
+++ ci/master/bin/build.sh
@@ -3,6 +3,7 @@
 set -e -v
 
 conda install -q -y -c defaults --override-channels conda=4.5 conda-build conda-verify
+conda install -y git pip --file https://raw.githubusercontent.com/bioconda/bioconda-utils/master/bioconda_utils/bioconda_utils-requirements.txt
 
 BUILD_DIR=$(ls -d -1 $(pwd)/build-*)
 CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
