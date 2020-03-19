563f3e8dae397a660caf4e25dfcc1db037cc081b
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-03-19 09:58:19 -0700

    Revert "HACK: test temporary iqtree builds"
    This reverts commit 80e2c27ec923fa6c2e9feba44b7cb6cf8c02e475.

--- ci/master/bin/integration.sh
+++ ci/master/bin/integration.sh
@@ -8,8 +8,6 @@ conda create -q -y -p ./test-env
 PKG_NAMES=$(cat $(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/$/\/version-spec.txt/" | xargs) | xargs)
 CHANNELS=$(ls -1 -d $(pwd)/* | grep '^.\+-channel$' | sed "s/^/ -c /" | xargs)
 conda install -p ./test-env -q -y $CHANNELS \
-  -c https://99276-42372094-gh.circle-artifacts.com/0/tmp/artifacts/packages \
-  -c https://99275-42372094-gh.circle-artifacts.com/0/tmp/artifacts/packages \
   -c https://conda.anaconda.org/conda-forge \
   -c https://conda.anaconda.org/bioconda \
   -c defaults \
