f11227aba39083af8a77b6269c5c2d571fd3274a
Matthew Dillon <matthewrdillon@gmail.com>
2021-02-17 08:37:15 -0700

    bug: missing quotes

--- extra/github/bin/copy-templates.sh
+++ extra/github/bin/copy-templates.sh
@@ -6,4 +6,4 @@ DEST=$(readlink -f $REPO-updated)
 git clone $REPO $DEST
 
 cd ./template-repo/github_templates
-./update_templates.sh $DEST $PACKAGE_NAME $ADDITIONAL_TESTS
+./update_templates.sh $DEST $PACKAGE_NAME '${ADDITIONAL_TESTS}'
