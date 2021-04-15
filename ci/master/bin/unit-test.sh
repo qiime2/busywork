e04fbf2675a26b4933af46abbdae5e9abcf73b9c
Matthew Dillon <matthewrdillon@gmail.com>
2021-04-15 08:21:12 -0700

    debug: echo env var

--- ci/master/bin/unit-test.sh
+++ ci/master/bin/unit-test.sh
@@ -9,6 +9,8 @@ conda env create -q -p ./test-env --file environment-files/$RELEASE/test/qiime2-
 set +v
 echo "source activate ./test-env"
 source activate ./test-env
+echo "running test cmd:"
+echo $TEST_CMD
 set -v
 
 $TEST_CMD
