af9ceeb09bc44c8e4cec77b86500f6977b8da20f
Matthew Dillon <matthewrdillon@gmail.com>
2021-04-29 14:54:36 -0700

    hack: bye bye bye

--- ci/master/bin/unit-test.sh
+++ ci/master/bin/unit-test.sh
@@ -6,6 +6,9 @@ conda upgrade -n base -q -y conda
 
 conda env create -q -p ./test-env --file environment-files/$RELEASE/test/qiime2-$RELEASE-py38-$PLATFORM-conda.yml
 
+# for q2galaxy
+rm -rf ./.planemo
+
 set +v
 echo "source activate ./test-env"
 source activate ./test-env
